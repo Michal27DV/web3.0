acd59ce2ad0152c34cfb61c2ea8b5cd0f084b672
Michal27DV <michalek.dvork@gmail.com>
2025-08-10 14:16:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+
+var firstName = "Michal";
+console.log(firstName);
+
+var lastName = "Dvořák";
+console.log(firstName, lastName)
+
+var age = "15";
+console.log(age);
+
+var job = "Žák";
+console.log(job);
+
+var adult = true;
+console.log(adult)
+
+var age2 = 25
+var age2 = "Michal"
+console.log(age2)
+*/
+
+confirm("Jsi plnolety?")
+
+
+var age = prompt("Kolik je ti:")
+console.log(age)
